a032ccc7545d7e2ecff433f37608213610ff7adb
xiapo00 <59460454+xiapo00@users.noreply.github.com>
2020-04-05 12:22:46 +0800

    更新Solution_0002（未完全解决）

--- Solutions.py
+++ Solutions.py
@@ -20,5 +20,53 @@ class Solution_0001:
         assert self.twoSum_2(nums, target) == [1, 2]
 
 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution_0002:
+    def addTwoNumbers_1(self, l1, l2):
+        # 此法失败，输入[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1], [5,6,4]时
+        # 输出不正确，判断为int类型溢出，即题意不允许使用int加法实现，必须自底层实现
+        n1 = 0
+        i = 1
+        while l1:
+            n1 += l1.val * i
+            i *= 10
+            l1 = l1.next
+        n2 = 0
+        i = 1
+        while l2:
+            n2 += l2.val * i
+            i *= 10
+            l2 = l2.next
+        n3 = n1 + n2
+        a = ListNode(n3 % 10)
+        c = a
+        n3 = int(n3 / 10)
+        while n3:
+            b = ListNode(n3 % 10)
+            n3 = int(n3 / 10)
+            a.next = b
+            a = b
+        return c
+    
+    def test(self):
+        a1 = ListNode(2)
+        a2 = ListNode(4)
+        a3 = ListNode(3)
+        a1.next = a2
+        a2.next = a3
+        b1 = ListNode(5)
+        b2 = ListNode(6)
+        b3 = ListNode(4)
+        b1.next = b2
+        b2.next = b3
+        # assert self.addTwoNumbers_1(a1, b1) == c1
+
+
 if __name__ == '__main__':
     Solution_0001().test()
+    Solution_0002().test()
